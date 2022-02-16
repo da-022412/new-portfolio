406394b2d1c8238b22d38c40696434984348c110
Dennis Acosta <email@dennisacosta.me>
2022-02-16 17:59:01 -0500

    Setup layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// import './App.css';
+
+// Components
+import Header from './layout/Header';
+import MobileHeader from './layout/MobileHeader';
+import Footer from './layout/Footer';
+
+import Home from './home/Home';
+
+const useMediaQuery = (width) => {
+    const [targetReached, setTargetReached] = useState(false);
+
+    const updateTarget = useCallback((e) => {
+        if (e.matches) {
+            setTargetReached(true);
+        } else {
+            setTargetReached(false);
+        }
+    }, []);
+
+    useEffect(() => {
+        const media = window.matchMedia(`(max-width: ${width}px)`);
+        media.addEventListener('change', updateTarget);
+
+        // Check on mount (callback is not called until a change occurs)
+        if (media.matches) {
+            setTargetReached(true);
+        }
+
+        return () => media.removeEventListener('change', updateTarget);
+    }, []);
+
+    return targetReached;
+};
+
+const links = [
+    {
+        id: 0,
+        title: 'My work',
+        slug: '#work',
+    },
+    {
+        id: 1,
+        title: 'Get in touch',
+        slug: '#contact',
+    },
+];
+
+const App = () => {
+    const isBreakpoint = useMediaQuery(980);
+
+    return (
+        <Router>
+            {isBreakpoint ? (
+                <MobileHeader links={links} />
+            ) : (
+                <Header links={links} />
+            )}
+            <Routes>
+                <Route path='/' element={<Home />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
+};
 
 export default App;
---
+++ src/layout/MobileHeader.js
@@ -0,0 +1,9 @@
+const MobileHeader = () => {
+    return (
+        <header>
+            <h1>Test</h1>
+        </header>
+    );
+};
+
+export default MobileHeader;
