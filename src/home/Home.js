d456e2397ddd45c1983937e53ea152cfd6f21702
Dennis Acosta <email@dennisacosta.me>
2022-02-17 17:50:30 -0500

    Rearrange home component

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,48 +1,7 @@
-import styled from '@emotion/styled';
-
-const font = 'gtWalsheimProRegular, sans-serif';
-
-const HeadingSection = styled.section`
-    box-sizing: border-box;
-    margin: auto;
-    max-width: 90%;
-    width: 1200px;
-`;
-
-const HeadingContainer = styled.div`
-    padding: 5rem 0 8.75rem;
-`;
-
-const Heading = styled.h1`
-    font-family: ${font};
-    font-size: 3.75rem;
-`;
-
-const SubHeading = styled.p`
-    font-family: ${font};
-    font-size: 1.25rem;
-    width: 50%;
-`;
-
-const Divider = styled.div`
-    background-color: #f7f7f7;
-    height: 2px;
-`;
+import Hero from './Hero';
 
 const Home = () => {
-    return (
-        <HeadingSection>
-            <HeadingContainer>
-                <Heading>I'm Dennis, Front-End Developer</Heading>
-                <SubHeading>
-                    Lorem ipsum dolor sit amet consectetur adipiscing elit sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Enim ad minim veniam quis nostrud
-                </SubHeading>
-            </HeadingContainer>
-            <Divider></Divider>
-        </HeadingSection>
-    );
+    return <Hero />;
 };
 
 export default Home;
