7fe41515cb536fbf2d3154f8fac5144de31728b4
Dennis Acosta <email@dennisacosta.me>
2022-02-24 14:47:35 -0500

    Reorder styles

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,14 +1,14 @@
 import styled from 'styled-components';
 
+const Heading = ({ level, ...rest }) => {
+    return <StyledHeading as={`h${level}`} {...rest} />;
+};
+
 const StyledHeading = styled.h1`
     color: var(--color-text);
     font-family: var(--primary-font);
     font-size: var(--heading-1);
 `;
-
-const Heading = ({ level, ...rest }) => {
-    return <StyledHeading as={`h${level}`} {...rest} />;
-};
 
 Heading.defaultProps = {
     level: 1,
