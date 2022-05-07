b962b094cbbdc5f7ca7d52825e3eea5c193911c7
Dennis Acosta <email@dennisacosta.me>
2022-05-07 14:28:44 -0400

    Update imports

--- src/Views.js
+++ src/Views.js
@@ -1,8 +1,4 @@
-import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import axios from 'axios';
-
-import { PROJECTS_API_URL } from './utils';
 
 // Views
 import Home from './views/Home';
